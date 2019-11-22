7ade46321e553c75ff861098a6f05a2167293116
vsn23kamei <vsn_2384108@i.softbank.jp>
2019-11-22 15:43:49 +0900

    user_detail修正

--- 01_基本設計/DDL/USER_DETAIL.sql
+++ 01_基本設計/DDL/USER_DETAIL.sql
@@ -1,17 +1,17 @@
-CREATE TABLE public."user_detail"
+CREATE TABLE public.user_detail
 (
-    "user_id" text COLLATE pg_catalog."default" NOT NULL,
-    "user_name" text COLLATE pg_catalog."default" NOT NULL,
-    "address" text COLLATE pg_catalog."default",
-    "tel_number" integer,
-    "mail_address" integer,
-    "add_timestamp" date NOT NULL,
-    "last_login_time" date NOT NULL,
-    "last_modified" date NOT NULL,
-    CONSTRAINT "user_detail_pkey" PRIMARY KEY ("user_id")
+    user_id text COLLATE pg_catalog."default" NOT NULL,
+    user_name text COLLATE pg_catalog."default" NOT NULL,
+    address text COLLATE pg_catalog."default",
+    tel_number text COLLATE pg_catalog."default",
+    mail_address text COLLATE pg_catalog."default",
+    add_timestamp date NOT NULL,
+    last_login_time date NOT NULL,
+    last_modified date NOT NULL,
+    CONSTRAINT user_detail_pkey PRIMARY KEY (user_id)
 )
 
 TABLESPACE pg_default;
 
-ALTER TABLE public."user_detail"
+ALTER TABLE public.user_detail
     OWNER to student;
